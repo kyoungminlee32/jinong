af5e3fac02de443d31597519e31910cc2cbdfb22
lee kyoungmin <kyoungminlee32@gmail.com>
2022-05-25 13:09:23 +0900

    전체 페이지 수정

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,11 @@
 function goBack(){
 	window.history.back();
 }
+
+$( document ).ready(function(){
+	$(".table-work .form-check-input").on({
+		change:function(e){
+			$(this).parents(".table-work tr").toggleClass('bg-info02');
+		}
+	});
+});
